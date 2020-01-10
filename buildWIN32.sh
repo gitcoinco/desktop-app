f8002c29dd463c62adbad7cccb69f1d7a85f5919
Owocki <ksowocki@gmail.com>
2020-01-10 08:48:06 -0700

    windows build script

---
+++ buildWIN32.sh
@@ -0,0 +1,2 @@
+electron-packager . gitcoin --overwrite --asar=true --platform=win32 --arch=ia32 --icon Icon/Icon.icns --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="Gitcoin"
+
